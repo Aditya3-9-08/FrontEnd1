03448a32fdd23a2b6325efde576e73f825c3aede
Aditya3-9-08 <adityaharis312@gmail.com>
2025-09-15 12:52:39 +0700

    rawr

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<head>
+<?php include "component/header.php" ?>
+</head>
+<body>
+    <?php include "component/navbar.php" ?>
+    <?php include "page/home.php" ?>
+    <?php include "component/footer.php" ?>
+</body>
+</html>
